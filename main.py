e976fb33573f3fc14600e3a41cb7b9c47966fb27
GunKate <gokhfeld.ev@gmail.com>
2024-07-18 12:11:14 +0500

    1 part dz

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def page1():
+    return render_template("index.html")
+
+@app.route("/dlog/")
+def page2():
+    return render_template("blog.html")
+
+@app.route("/contacts/")
+def page3():
+    return render_template("contacts.html")
+
+@app.route("/datatime/")
+def page():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return f"Hello World! Current date and time is: {current_time}"
 
-@app.route("/new/")
-def new():
-    return "new page"
 
 if __name__ == "__main__":
     app.run()
