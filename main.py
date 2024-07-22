1323c26f3cbd1e511bf58543af1e243f8c1147aa
GunKate <gokhfeld.ev@gmail.com>
2024-07-22 23:17:45 +0500

    update main

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def food():
-    context = {
-        "link": "Перейти в кинотеатр"
-    }
+
     return render_template("home.html", **context)
 
 @app.route("/food/")
 def foodGood():
-    context = {
-        "link": "Перейти в кинотеатр"
-    }
+
     return render_template("about.html", **context)
 
 if __name__ == "__main__":
